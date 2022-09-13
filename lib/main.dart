278f2cd8b597ebe2008b1b865a00514f84876273
Stuti Singhal <Stuti.Singhal@INFOVISIONLABS.COM>
2022-09-13 12:07:57 +0530

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -8,92 +8,55 @@ void main() {
 class MyApp extends StatefulWidget {
   // ignore: prefer_const_constructors_in_immutables
   MyApp({super.key});
-
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  String firstButtonName = "Press";
-  String secondButtonName = "Press Me";
-  int changingIndex = 0;
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text("WELCOME!!")),
-        ),
-        body: Center(
-          child: changingIndex == 0
-              ? Container(
-                  width: double.infinity,
-                  // height: double.infinity,
-                  color: Colors.grey,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.pink,
-                          onPrimary: Colors.white,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            firstButtonName = "Pressed";
-                          });
-                        },
-                        child: Text(firstButtonName),
-                      ),
-                      ElevatedButton(
-                        // style: const ButtonStyle( backgroundColor: Colors.yellow),
-                        onPressed: () {
-                          setState(() {
-                            secondButtonName = "I'm Pressed";
-                          });
-                        },
-                        child: Text(secondButtonName),
-                      ),
-                    ],
-                  ),
-                )
-              : Image.asset("assets/images/nature.jpeg"),
-          // : Image.network(
-          //     'https://picsum.photos/id/1074/400/400',
-          //   ),
-          // : Image.file(
-          //     File('/storage/emulated/0/oats.jpeg'),
-          //   ),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.home,
-                // color: Color.fromARGB(255, 194, 26, 236),
-                size: 30,
-              ),
-              label: "HOME",
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(
-                  Icons.person,
-                  // color: Color.fromARGB(255, 240, 151, 181),
-                  size: 30,
-                ),
-                label: "PROFILE")
-          ],
-          currentIndex: changingIndex,
-          onTap: (int index) {
-            setState(() {
-              changingIndex = index;
-            });
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: FirstPage());
+  }
+}
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text("WELCOME!!")),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.pink,
+            onPrimary: Colors.white,
+          ),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (BuildContext context) {
+                return const SecondPage();
+              }),
+            );
           },
+          child: const Text("Next"),
         ),
       ),
     );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  const SecondPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            //this is to remove back button which is bydefault provided
+            // automaticallyImplyLeading:false,
+            ));
   }
 }
